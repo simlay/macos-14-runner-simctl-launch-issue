265f7f0907ec84c60e2912946175c6d6eeff2cc1
Sebastian Imlay <sebastian.imlay@gmail.com>
2024-02-01 10:51:07 -0500

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Sebastian.Imlay on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
